0436c73c9bd6af24fa23f58258496a32ac324daf
erickeduarte <erickedur@gmail.com>
2013-09-19 10:43:00 -0600

    fishcom2

--- Proyecto/include/image.hh
+++ Proyecto/include/image.hh
@@ -1,6 +1,6 @@
 #include "../include/CImg.h"
 #include <string>
-
+///Se incluye la librería CImg.
 using namespace cimg_library;
 using namespace std;
 
@@ -10,25 +10,33 @@ using namespace std;
 class Image
 {
 private:
+	///Primero se declaran las variables de la clase como privadas
 	CImg<unsigned char> *Img;
+	///La variable *Img es un puntero a la imagen del tipo CImg y en CImg se trata esta como un unsigned char.
 	unsigned int width;
 	unsigned int height;
 	unsigned int depth;
 	unsigned int spectrum;
-	
+	///Las variables width, height, depth y spectrum se refieren a las dimensiones y se tratan como unsigned int
 	
 public:
+	///Luego se declaran las funciones de la clase como publicas
 	Image ();
+	///Image() es el primer constructor de la clase que no recibe parámetros.
 	Image (const char *const filename);
+	///Image (const char *const filename) es el constructor de la clase que recibe solo *filename
 	Image (const unsigned int width, const unsigned int height, const unsigned int depth, const unsigned int spectrum, int value);
+	///Image (const unsigned int width, const unsigned int height, const unsigned int depth, const unsigned int spectrum, int value)
+	///Es el constructor que recibe las dimensiones de la imagen(width, height, depth, spectrum) y un int value QUE NO SE QUE ES.
 	~Image(void);
-	
-	unsigned int get_width();
-	unsigned int get_height();
-	unsigned int get_depth();
-	unsigned int get_spectrum();
+	///~Image(void) es el destructor de la clase
+	unsigned int get_width();///get_width permite obtener el ancho de la imagen
+	unsigned int get_height();///get_height permite obtener el alto de la imagen
+	unsigned int get_depth();///get_depth permite obtener el número de capas de color que tiene la imagen
+	unsigned int get_spectrum();///get_spectrum permite obtener spectrum(NO SE)
 	
 	void save(const char *const savefilename);
+	///save(const char *const savefilename) permite salvar una imagen con el nombre de "savefilename"
 	
 };
 
--- Proyecto/src/image.cpp
+++ Proyecto/src/image.cpp
@@ -2,15 +2,23 @@
 
 Image::Image(const char *const filename)
 {
+	///Este es el constructor de la clase en donde solamente se conoce la variable filename(NO ESTOY SEGURO DE QUE ES!!)
 	this->Img = new CImg<unsigned char>(filename);
+	///La variable Img es del tipo CImg y se trata como un unsigned char.
 	this->width = this->Img->width();
+	///width se refiere al ancho de la imagen.
 	this->height = this->Img->height();
+	///height se refiere al alto de la imagen.
 	this->depth = this->Img->depth();
+	///depth se refiere a la cantidad de capas que tenga la imagen
 	this->spectrum = this->Img->spectrum();
+	///NO SE QUE ES SPECTRUM
 }
 
 Image::Image()
 {
+	///Este es el constructor de la clase que inicializa las variables en 0.
+	///En el caso de Img se llama al constructor de CImg que inicialice la imagen como vacía.
 	this->Img = new CImg<unsigned char>();
 	this->width = 0;
 	this->height = 0;
@@ -20,7 +28,9 @@ Image::Image()
 
 Image::Image(const unsigned int width, const unsigned int height, const unsigned int depth, const unsigned int spectrum, int value)
 {
+	///Este es el constructor de la imagen para el caso en el que le pasen los parámetros de la imagen.
 	this->Img = new CImg<unsigned char>(width, height, depth, spectrum, value);
+	///Llama al constrcutor de CImg donde se le dan los valores de width, height, depth, spectrum, y value (NO SE QUE ES VALUE)
 	this->width = width;
 	this->height = height;
 	this->depth = depth;
@@ -29,42 +39,48 @@ Image::Image(const unsigned int width, const unsigned int height, const unsigned
 
 Image::~Image(void)
 {
-
+	///~Image(void) es el destructor de la clase
 }
 
 unsigned int Image:: get_width()
 {
+	///get_width retorna un puntero entero con el ancho de la imagen
 	return this->width;
 }
 unsigned int Image:: get_height()
 {
+	///get_height retorna un puntero entero con el alto de la imagen
 	return this->height;
 }
 unsigned int Image:: get_depth()
 {
+	///get_depth retorna un puntero entero con el número de capas o canales de la imagen
 	return this->depth;
 }
 unsigned int Image:: get_spectrum()
 {
+	///get_spectrum retorna un puntero entero con spectrum (NO SE)
 	return this->spectrum;
 }
 
 void Image:: save(const char *const savefilename)
 {
+	///save permite salvar un archivo con la imagen con la que se está trabajando.
 	this->Img->save(savefilename);
 }
 
 
 /*
  * Filter Function
+ * ///Esta función recorre toda la matriz con un kernel de dimensión desconocida y aplica una función cualquiera a los píxeles de la imagen cuando el kernel esta encima de ellos, el kernel va recorriendo toda la imagen haciendo esto.
  * void Filter (int [] kernel)
  * {
 	 * // K = 2m+1;
 	 * int k = matriz.width();
 	 * int m = (k-1)/2
 	 * 
-	 * //Se define la matriz de números (Para extraer los valores de unsigned char)
-	 * //Se definen las dimensiones de la 
+	 * ///Se define la matriz de números (Para extraer los valores de unsigned char)
+	 * ///Se definen las dimensiones de la 
 	 * unsigned char matrix [this->spectrum()] [this->depth()] [this->width()] [this->height()];
 	 * 
 	 * 
@@ -76,6 +92,7 @@ void Image:: save(const char *const savefilename)
 			 * {
 				 * for(int y = 0; y < this->height(); y++)
 				 * {
+				 *  ///Se recorre la matriz en sus 4 dimensiones, con 4 ciclos, luego se debe recorrer el Kernel con otros 2 ciclos.
 					 * //HASTA AQUI, SOLO RECORRO TODA LA IMAGEN; FALTA EL KERNEL
 					 * 
 					 * int sum = 0;
