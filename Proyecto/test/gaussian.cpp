a41380e51bedd01c4ec9fafdd5d1891d7fdff015
frinconr <frinconr95@gmail.com>
2013-09-25 23:18:56 -0600

    gaussian

--- Proyecto/test/gaussian.cpp
+++ Proyecto/test/gaussian.cpp
@@ -1,21 +1,37 @@
 #include "../include/image.hh"
 #include "math.h"
+#include "iostream"
+
+using namespace std;
 
 int main()
 {
 	int o=1;
 	int dim=3;
-	double = kernel[dim*dim];
+	double kernel[dim*dim];
+	
+	int m = (dim-1)/2;
 	
 	double gaussian =1/(3.1415*2*o);
-	
+	//cout<< gaussian<<endl;
 	double G;
+
+	for(int i =-m; i <=m; i++)
+						{
+							for(int j =-m; j<=+m; j++)
+							{
+								double exp= -(j*j+i*i)/(2*(o*o));
+								kernel[(i+m)*dim + (j+m)]=gaussian*pow(2.7,exp); 
+								cout<<"exp : " << exp<< endl;
+								cout<< kernel[(i+m)*dim + (j+m)]<<endl;
+							}
+						}
+						
+/*	for (int j=0; j<dim*dim; j++)
+	{ 
+		cout<< kernel[j]<<endl;
 	
-	for (int j=-m; j<=m; j++)
-	{ 		for (int i=-m; i<=m; i++)
-		{
-		kernel[5*j+i] = 1;
-		}
 	}
+*/
 
 }
