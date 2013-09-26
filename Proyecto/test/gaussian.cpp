ae273a922196fb78d42e6f4061237d12557a5bee
frinconr <frinconr95@gmail.com>
2013-09-26 00:16:03 -0600

    gaussian

--- Proyecto/test/gaussian.cpp
+++ Proyecto/test/gaussian.cpp
@@ -1,37 +1,31 @@
 #include "../include/image.hh"
-#include "math.h"
 #include "iostream"
 
 using namespace std;
 
 int main()
 {
-	int o=1;
+/*	int o=1;
 	int dim=3;
 	double kernel[dim*dim];
 	
 	int m = (dim-1)/2;
 	
-	double gaussian =1/(3.1415*2*o);
+	double gaussian =1/pow(3.1415*2*o,0.5);
 	//cout<< gaussian<<endl;
-	double G;
 
 	for(int i =-m; i <=m; i++)
-						{
-							for(int j =-m; j<=+m; j++)
-							{
-								double exp= -(j*j+i*i)/(2*(o*o));
-								kernel[(i+m)*dim + (j+m)]=gaussian*pow(2.7,exp); 
-								cout<<"exp : " << exp<< endl;
-								cout<< kernel[(i+m)*dim + (j+m)]<<endl;
-							}
-						}
-						
-/*	for (int j=0; j<dim*dim; j++)
-	{ 
-		cout<< kernel[j]<<endl;
-	
+	{
+		for(int j =-m; j<=+m; j++)
+		{
+			double exp= -(i*i+j*j)*0.5/o;
+			kernel[(i+m)*dim + (j+m)]=gaussian*pow(2.7,exp); 
+			cout<<"exp : " << exp<< endl;
+			cout<< kernel[(i+m)*dim + (j+m)]<<endl;
+		}
 	}
-*/
-
+*/	
+	Image imagen ("../../Multimedia/taj_orig.jpg");
+	Image filtered = imagen.filter_gaussian(1,3);
+	filtered.save("gaussian_taj.ppm");
 }
